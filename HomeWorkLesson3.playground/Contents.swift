2e570bb7b2946ef2358a5797866de732d0eed7b3
Sergey Kuchik <kpartsby@gmail.com>
2020-09-16 23:13:31 +0300

    two commit

---
+++ HomeWorkLesson3.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+var str = "Hello, playground"
+
+struct Car{
+    var brand: String
+    var model: String
+    var year : Int
+    var cubicCapacity: Int
+    var fuelType: String
+    var typeGear: String
+  
+    
+   
+
+    init ( brand: String, model: String, year: Int, cubicCapacity: Int, fuelType: String, typeGear: String){
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.cubicCapacity = cubicCapacity
+        self.fuelType = fuelType
+        self.typeGear = typeGear
+        
+    
+    }
+    func carInfo (){
+        }
+}
+let auto = Car (brand: "Mercedes-Benz", model: "E-class", year: 2005, cubicCapacity: 2200, fuelType: "Diesel", typeGear: "Automatic")
+auto.brand
+auto.model
+
+auto.carInfo()
+
+    
+    
